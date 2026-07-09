05b9ef9ad7587f58f1da3b58b1e1785b70ff0ca0
fAITH WANJIRU <wanjirufaith842@gmail.com>
2026-07-09 10:06:55 +0300

    Update database credentials in config.php

--- config.php
+++ config.php
@@ -1,19 +1,14 @@
 <?php
-
 $host = "localhost";
-$user = "root";
-$password = "";
+$user = "your_database_username";
+$password = "your_database_password";
 $database = "studentidtracker122";
 
-// Create connection
 $conn = mysqli_connect($host, $user, $password, $database);
 
-// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Set character encoding
 mysqli_set_charset($conn, "utf8");
-
 ?>
